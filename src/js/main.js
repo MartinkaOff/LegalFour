e964e9a16498ceec18a22bccabdf47cda3199160
MartinkaOff <110162487+MartinkaOff@users.noreply.github.com>
2023-02-10 11:42:11 +0600

    Add header, promo, services section

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import slider from './slider';
+
+window.addEventListener('DOMContentLoaded', () => {
+    /* slider(); */
+});
---
+++ src/js/slider.js
@@ -0,0 +1,12 @@
+const slider = () => {
+    let slider = tns({
+        "container": ".services__items",
+        "items": 3,
+        "swipeAngle": false,
+        "speed": 400,
+        "controls": false,
+        "navAsThumbnails": false
+    });
+}
+
+export default slider;
